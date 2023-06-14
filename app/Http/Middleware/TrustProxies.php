24f7ad612bb1fff832a610d72a24193754434150
soma.kato <k.sinnou.sitennnou.ryo@gmail.com>
2023-06-14 01:53:02 +0000

    laravel breeze daisyui

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies ='*';    //全プロキシを信用するように変更
+    protected $proxies ;    
 
     /**
      * The headers that should be used to detect proxies.
